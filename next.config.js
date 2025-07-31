73d03863714878da2e25cb91d05e425c29e6ca33
Christian Pezzin <christian.pezzin@gmail.com>
2025-07-31 05:26:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  experimental: {
+    serverComponentsExternalPackages: ['@simplewebauthn/server']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: false,
+        stream: false,
+        buffer: false,
+      };
+    }
+    return config;
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
